8907218c5deb18588c5cac92be7fa17152b7ed26
Federico Ziliotto <federico.ziliotto.1@studenti.unipd.it>
2015-09-07 11:10:39 +0200

    First commit

---
+++ Assignment1/Code/syn2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+void display(char *str) {
+    char *tmp;
+    for (tmp = str; *tmp; tmp++) {
+        write(1, tmp, 1);
+        usleep(100);
+    }
+}
+int main() {
+    int i;
+    struct sembuf ab_up =   {0, 1, 0};
+    struct sembuf ab_down = {0, -1, 0};
+    struct sembuf cd_up =   {0, 1, 0};
+    struct sembuf cd_down = {0, -1, 0};
+
+    int sem;
+
+    sem = semget(IPC_PRIVATE, 2, 0600); /* create semaphore */
+    if (sem == -1) {
+        perror ("semget:");
+    }
+    semop(sem, &ab_up, 1);
+
+    if (fork()) {
+        for (i = 0; i < 10; i++) {
+            semop(sem, &ab_down, 1);
+            display("ab");
+            semop(sem, &cd_up, 1);
+        }
+        wait(NULL);
+    } else {
+        for (i = 0; i < 10; i++) {
+            semop(sem, &cd_down, 1);
+            display("cd\n");
+            semop(sem, &ab_up, 1);
+        }
+    }
+    return 0;
+}
