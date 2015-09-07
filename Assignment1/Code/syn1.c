38070a11c972120921bbf26bd73b864acff46f48
Stefano Sandonà <sando92@hotmail.com>
2015-09-07 11:51:30 +0200

    syn1

---
+++ Assignment1/Code/syn1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+
+void display(char *str) {
+    char *tmp;
+    for (tmp = str; *tmp; tmp++) {
+        write(1, tmp, 1);
+        usleep(100);
+    }
+}
+
+int main() {
+    int i;
+    int mutex = semget(IPC_PRIVATE, 1, 0600);
+    struct sembuf up =   {0, 1, 0};
+    struct sembuf down = {0, -1, 0};
+    semop(mutex, &up, 1);
+    if (fork()) {
+        for (i = 0; i < 10; i++) {
+            semop(mutex, &down, 1);
+            display("Hello world\n");
+            semop(mutex, &up, 1);
+        }
+        wait(NULL);
+    } else {
+        for (i = 0; i < 10; i++) {
+            semop(mutex, &down, 1);
+            display("Bonjour monde\n");
+            semop(mutex, &up, 1);
+        }
+    }
+    return 0;
+}
