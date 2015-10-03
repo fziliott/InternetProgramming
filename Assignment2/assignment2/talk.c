b7756b5712d203b8938b6a8ae92992c8d0ac8e3d
Federico Ziliotto <federico.ziliotto.1@studenti.unipd.it>
2015-10-03 10:09:27 +0200

    talk a caratteri

--- Assignment2/assignment2/talk.c
+++ Assignment2/assignment2/talk.c
@@ -1,3 +1,16 @@
+#include <pthread.h>
+#include <curses.h>
+#include <termios.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -8,17 +21,51 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
 
 #define BACKLOG 5
-#define PORT 5553
-#define NB_PROC 10
+#define PORT 5555
 
-int socketfd;
+int done = 0;
+int sockfd;
+
+WINDOW *receive_win;
+WINDOW *send_win;
+int receive_win_line = 1;
+int send_win_line = 1;
+int width, height;
+
+void *send_message();
+
+void *get_message();
 
 void handler() {
-    close(socketfd);
-    exit(1);
+    close(sockfd);
+    wclear(receive_win);
+    wclear(send_win);
+    exit(0);
 }
+
+ssize_t writen(int fd, const void *vptr, size_t n) {
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if  ((nwritten = write(fd, ptr, nleft)) <= 0 ) {
+            if (errno == EINTR)
+                nwritten = 0; /* and call write() again */
+            else
+                return -1; /* error */
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return n;
+}
+
 
 int openSocket(struct sockaddr_in *server_addr) {
     int fd;
@@ -50,67 +97,31 @@ int openSocket(struct sockaddr_in *server_addr) {
     return fd;
 }
 
-
-ssize_t writen(int fd, const void *vptr, size_t n) {
-    size_t nleft;
-    ssize_t nwritten;
-    const char *ptr;
-    ptr = vptr;
-    nleft = n;
-    while (nleft > 0) {
-        if  ((nwritten = write(fd, ptr, nleft)) <= 0 ) {
-            if (errno == EINTR)
-                nwritten = 0; /* and call write() again */
-            else
-                return -1; /* error */
-        }
-        nleft -= nwritten;
-        ptr += nwritten;
-    }
-    return n;
-}
-
-void sig_chld(int a) {
-    while (waitpid(0, NULL, WNOHANG) > 0) {}
-    close(socketfd);
-    exit(1);
-}
-
-void treat_request_read() {
-    char leggi[500];
-    while(1) {
-        if((recv(socketfd, leggi, 500, 0)) > 0) {
-            printf("\nOther: %s", leggi);
-        } else {
-            handler();
-            exit(1);
-        }
-    }
-
-}
-
-void treat_request_write() {
-    char *msg;
-    char enter[100] = "";
-    size_t size = 0;
-
-    while(1) {
-        printf("\nMe: ");
-        if(getline(&msg, &size, stdin) == -1) {
-            printf("Couldn't read the input\n");
-            handler();
-            exit(1);
-        }
-        writen(socketfd, (void *)msg, strlen(msg) + 1);
-    }
-}
-
 int main(int argc, char **argv) {
+
+    /*windows setting*/
+    initscr();
+    cbreak();
+    noecho();
+    //keypad(send_win, 1);
+    //keypad(receive_win, 1);
+    getmaxyx(stdscr, height, width);
+    receive_win = newwin(height / 2, width, 0, 0);
+    send_win = newwin(height / 2, width, height / 2, 0);
+    wborder(receive_win,' ',' ',' ','_',' ',' ',' ',' ');
+    scrollok(receive_win, TRUE);
+    scrollok(send_win, TRUE);
+    wsetscrreg(receive_win, 1, height / 2 - 2);
+    wsetscrreg(send_win, 1, height / 2 - 2);
+    wrefresh(receive_win);
+    wrefresh(send_win);
+
+    /*client*/
     if(argc == 2) {
         struct sockaddr_in remote;
         struct hostent *host;
 
-        if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
             perror("socket");
             exit(1);
         }
@@ -134,37 +145,98 @@ int main(int argc, char **argv) {
         remote.sin_port = htons(PORT);
         remote.sin_addr.s_addr = inet_addr(addr[0]);
         printf("Addr: %s, PORT: %d\n", addr[0], PORT);
-        if (connect(socketfd, (struct sockaddr *)&remote, sizeof(struct sockaddr_in)) == -1) {
+        if (connect(sockfd, (struct sockaddr *)&remote, sizeof(struct sockaddr_in)) == -1) {
             perror("connect");
             exit(1);
         }
-    } else {
+    } else {    /*server*/
         int num;
         struct sockaddr_in server_addr, client_addr;;
-        socketfd = openSocket(&server_addr);
-        signal(SIGINT, handler);
+        sockfd = openSocket(&server_addr);
         int yes = 1;
-        /*ignore 'address already in use'*/
-        if ( setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1 ) {
+        /*ignore address already in use'*/
+        if ( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1 ) {
             perror("setsockopt");
         }
-        if ( setsockopt(socketfd, SOL_SOCKET, SO_REUSEPORT, &yes, sizeof(int)) == -1 ) {
+        /*ignore port already in use'*/
+        if ( setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &yes, sizeof(int)) == -1 ) {
             perror("setsockopt");
         }
         int addrlen = sizeof(struct sockaddr_in);
-        socketfd = accept(socketfd, (struct sockaddr *) &client_addr, &addrlen);
-        printf("Request\n");
-        if(socketfd < 0)
-            exit(1);
+        sockfd = accept(sockfd, (struct sockaddr *) &client_addr, &addrlen);
+        //printf("Request\n");
+        if(sockfd < 0) {
+            handler();
+        }
     }
-
 
     signal(SIGINT, handler);
 
+    pthread_t threads[2];
+    void *status;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    //pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-    if (fork() == 0) {
-        treat_request_write();
-    } else treat_request_read();
+    pthread_create(&threads[0], &attr, send_message, NULL);
+    pthread_create(&threads[1], &attr, get_message, NULL);
 
-    return 0;
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+
+}
+
+
+void *send_message() {
+
+    char message[2];
+    int pos=0;
+    while(1) {
+        bzero(message, 2);
+        wrefresh(receive_win);
+        wrefresh(send_win);
+        int c = wgetch(send_win);
+        //mvwgetstr(send_win, send_win_line, 2, message);
+        if((c>31 && c <127) || c ==10){        
+
+  
+            sprintf(message, "%c", c);
+            wprintw(send_win,"%c", c);
+            //wmove(send_win, send_win_line,pos);
+
+            writen(sockfd, message, 2);
+            
+      
+        }  
+ 
+        
+
+    }
+}
+
+
+void *get_message() {
+    char message[2];
+    int pos = 0;
+    while(1) {
+        bzero(message, 2);
+        wrefresh(receive_win);
+        wrefresh(send_win);
+
+        if((recv(sockfd, message, 2, 0)) <= 0) {
+            handler();
+            exit(1);
+        }
+
+        mvwprintw(receive_win, receive_win_line, pos, message);
+        pos++;
+        if(pos > width || message[0]=='\n'){
+            receive_win_line++;
+            pos=0;
+        }
+        if(receive_win_line >= height / 2 - 2)
+         scroll(receive_win);
+
+
+    }
 }
