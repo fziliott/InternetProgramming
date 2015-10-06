2b06cc10c8a140f029be1263fb0161f7bff6ee6f
Stefano Sandonà <sando92@hotmail.com>
2015-10-06 17:00:17 +0200

    talk (reverted from commit fb7b71d105ec07963fbff347be31faff01ea28a3)

--- Assignment2/assignment2/talk.c
+++ Assignment2/assignment2/talk.c
@@ -2,6 +2,7 @@
 #include <curses.h>
 #include <termios.h>
 #include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -9,6 +10,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -28,29 +30,23 @@
 int done = 0;
 int sockfd;
 
-WINDOW *receive_win;        //window for display received messages
-WINDOW *send_win;           //window for display sent messages
-int receive_win_line = 1;   //cursor line for receive_win
-int send_win_line = 1;      //cursor line for send_win
-int width, height;          //height and width of the available screen area
+WINDOW *receive_win;
+WINDOW *send_win;
+int receive_win_line = 1;
+int send_win_line = 1;
+int width, height;
 
 void *send_message();
 
 void *get_message();
 
-/*finalization function, for a safe closure*/
 void handler() {
     close(sockfd);
     wclear(receive_win);
     wclear(send_win);
-    wclear(stdscr);
-    delwin(send_win);
-    delwin(receive_win);
-    endwin();
     exit(0);
 }
 
-/*function to assure the send of all needed bytes*/
 ssize_t writen(int fd, const void *vptr, size_t n) {
     size_t nleft;
     ssize_t nwritten;
@@ -70,7 +66,7 @@ ssize_t writen(int fd, const void *vptr, size_t n) {
     return n;
 }
 
-/*socket creation, bind and listen*/
+
 int openSocket(struct sockaddr_in *server_addr) {
     int fd;
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -107,10 +103,12 @@ int main(int argc, char **argv) {
     initscr();
     cbreak();
     noecho();
+    //keypad(send_win, 1);
+    //keypad(receive_win, 1);
     getmaxyx(stdscr, height, width);
     receive_win = newwin(height / 2, width, 0, 0);
     send_win = newwin(height / 2, width, height / 2, 0);
-    wborder(receive_win, ' ', ' ', ' ', '_', ' ', ' ', ' ', ' ');
+    wborder(receive_win,' ',' ',' ','_',' ',' ',' ',' ');
     scrollok(receive_win, TRUE);
     scrollok(send_win, TRUE);
     wsetscrreg(receive_win, 1, height / 2 - 2);
@@ -146,6 +144,7 @@ int main(int argc, char **argv) {
         remote.sin_family = AF_INET;
         remote.sin_port = htons(PORT);
         remote.sin_addr.s_addr = inet_addr(addr[0]);
+        printf("Addr: %s, PORT: %d\n", addr[0], PORT);
         if (connect(sockfd, (struct sockaddr *)&remote, sizeof(struct sockaddr_in)) == -1) {
             perror("connect");
             exit(1);
@@ -165,6 +164,7 @@ int main(int argc, char **argv) {
         }
         int addrlen = sizeof(struct sockaddr_in);
         sockfd = accept(sockfd, (struct sockaddr *) &client_addr, &addrlen);
+        //printf("Request\n");
         if(sockfd < 0) {
             handler();
         }
@@ -176,6 +176,7 @@ int main(int argc, char **argv) {
     void *status;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
+    //pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
     pthread_create(&threads[0], &attr, send_message, NULL);
     pthread_create(&threads[1], &attr, get_message, NULL);
@@ -185,24 +186,35 @@ int main(int argc, char **argv) {
 
 }
 
-/* code for the thread that manage the send of the messages*/
+
 void *send_message() {
+
     char message[2];
-    int pos = 0;
+    int pos=0;
     while(1) {
         bzero(message, 2);
         wrefresh(receive_win);
         wrefresh(send_win);
         int c = wgetch(send_win);
-        if((c > 31 && c < 127) || c == 10) {
+        //mvwgetstr(send_win, send_win_line, 2, message);
+        if((c>31 && c <127) || c ==10){        
+
+  
             sprintf(message, "%c", c);
-            wprintw(send_win, "%c", c);
+            wprintw(send_win,"%c", c);
+            //wmove(send_win, send_win_line,pos);
+
             writen(sockfd, message, 2);
-        }
+            
+      
+        }  
+ 
+        
+
     }
 }
 
-/* code for the thread that manage the receive of the messages*/
+
 void *get_message() {
     char message[2];
     int pos = 0;
@@ -218,11 +230,13 @@ void *get_message() {
 
         mvwprintw(receive_win, receive_win_line, pos, message);
         pos++;
-        if(pos > width || message[0] == '\n') {
+        if(pos > width || message[0]=='\n'){
             receive_win_line++;
-            pos = 0;
+            pos=0;
         }
         if(receive_win_line >= height / 2 - 2)
-            scroll(receive_win);
+         scroll(receive_win);
+
+
     }
 }
