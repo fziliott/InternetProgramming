68b34342e25643fb72b8a40513e894b5dafcdaed
Federico Ziliotto <federico.ziliotto.1@studenti.unipd.it>
2015-10-09 19:04:39 +0200

    hotel folder

---
+++ Assignment3/assignment3/hotel/Hotel.java
@@ -0,0 +1,6 @@
+import java.lang.String;
+
+public interface Hotel extends java.rmi.Remote {
+     boolean book(int roomType, String guest) throws java.rmi.RemoteException;
+     String listRooms() throws java.rmi.RemoteException;
+}
---
+++ Assignment3/assignment3/hotel/HotelServer.java
@@ -0,0 +1,16 @@
+import java.rmi.Naming;
+
+public class HotelServer{
+   public HotelServer() {
+     try {
+       Hotel c = new HotelImpl();
+       Naming.rebind("rmi://localhost/Hotel", c);
+     } catch (Exception e) {
+       System.out.println("Trouble: " + e);
+     }
+   } 
+
+   public static void main(String args[]) {
+     new HotelServer();
+   }
+}
